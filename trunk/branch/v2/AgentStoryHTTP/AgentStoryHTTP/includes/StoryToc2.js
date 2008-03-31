3f861ebf4e429b9283cfdf3774bc2a86f3093828
grantsteinfeld <grantsteinfeld@fa8b5a9b-b147-0410-89df-73272f86767a>
2008-03-31 15:46:40 +0000

    Replay Concept fix: working along to having the timer.js sorted out, it is been used inefficiently!

--- trunk/branch/v2/AgentStoryHTTP/AgentStoryHTTP/includes/StoryToc2.js
+++ trunk/branch/v2/AgentStoryHTTP/AgentStoryHTTP/includes/StoryToc2.js
@@ -1,53 +1,7 @@
 /*
 
     AgentIdea - Story toc login page
-    Copyright AgentIdea 2007
-    
-    please note this is private and a copyrighted original work by Grant Steinfeld.
-    
-    
-    
-1. Registration Number:    TXu-959-906  
-Title:    AgentIdea application studio for IE : version 1.0. 
-Description:    Computer program. 
-Note:    Printout only deposited. 
-Claimant:    AgentIdea, LLC 
-Created:    2000 
-
-Registered:    10Jul00
-
-Author on © Application:    text of computer program: Grant Steinfeld , 1967- & Oren Kredo , 1966-. 
-Special Codes:   1/C 
-
---------------------------------------------------------------------------------
-2. Registration Number:    TXu-960-165  
-Title:    AgentIdea application studio for Java : version 1.0. 
-Description:    Computer program. 
-Note:    Printout only deposited. 
-Claimant:    AgentIdea, LLC 
-Created:    2000 
-
-Registered:    10Jul00
-
-Author on © Application:    program text: Grant Steinfeld , 1967-, & Oren Kredo , 1966-. 
-Special Codes:   1/C 
-
---------------------------------------------------------------------------------
-3. Registration Number:    TXu-961-904  
-Title:    AgentIdea application studio for IIS : version 1.0 / authors, Grant Steinfeld, Oren Kredo. 
-Description:    Computer program. 
-Note:    Printout only deposited. 
-Claimant:    cAgentIdea, LLC 
-Created:    2000 
-
-Registered:    10Jul00
-
-Special Codes:   1/C 
-
-to verify search for Grant Steinfeld or Oren Kredo
-http://www.copyright.gov/records/cohm.html
-
-*/
+ */
 
 
 function StoryToc(aStories,aAttachPoint,aKlubName,loginCallBack)
@@ -101,10 +55,23 @@ function StoryToc(aStories,aAttachPoint,aKlubName,loginCallBack)
             storyTocItem.title = " by " + story.Author + " viewed " + story.UniqueHits + " times";
             anchorText.appendChild( storyTocItemText );
             storyTocItem.appendChild( anchorText );
+            
+             var anchorViewItem = document.createElement("DIV");
+             anchorViewItem.className="clsStoryTitle";
+            var anchorViewText2 = document.createElement("A");
+            anchorViewText2.href = "./StoryReplay.aspx?StoryID=" + story.ID;
+             var viewReplayTXT = document.createTextNode(" (replay...)");
+             anchorViewText2.appendChild(viewReplayTXT);
+             
+            anchorViewItem.appendChild( anchorViewText2 );
+            
+            storyTocItem.appendChild( anchorViewItem );
+            
             storyTocValues.push(storyTocItem);
+             
         } 
         
-        var oStoryTocGrid = newGrid2("storyTocGrid", totalNumberStories ,1,storyTocValues);
+        var oStoryTocGrid = newGrid2("storyTocGrid", storyTocValues.length ,1,storyTocValues,1);
         oStoryTocGrid.init( oStoryTocGrid );
 
         outerGridValues.push( oStoryTocGrid.gridTable );
--- trunk/branch/v2/AgentStoryHTTP/AgentStoryHTTP/includes/timer.js
+++ trunk/branch/v2/AgentStoryHTTP/AgentStoryHTTP/includes/timer.js
@@ -1,53 +1,8 @@
 ﻿/*
 
     AgentIdea - Story timer
-    Copyright AgentIdea 2007
-    
-    please note this is private and a copyrighted original work by Grant Steinfeld.
-    
-    
-    
-1. Registration Number:    TXu-959-906  
-Title:    AgentIdea application studio for IE : version 1.0. 
-Description:    Computer program. 
-Note:    Printout only deposited. 
-Claimant:    AgentIdea, LLC 
-Created:    2000 
-
-Registered:    10Jul00
-
-Author on © Application:    text of computer program: Grant Steinfeld , 1967- & Oren Kredo , 1966-. 
-Special Codes:   1/C 
-
---------------------------------------------------------------------------------
-2. Registration Number:    TXu-960-165  
-Title:    AgentIdea application studio for Java : version 1.0. 
-Description:    Computer program. 
-Note:    Printout only deposited. 
-Claimant:    AgentIdea, LLC 
-Created:    2000 
-
-Registered:    10Jul00
-
-Author on © Application:    program text: Grant Steinfeld , 1967-, & Oren Kredo , 1966-. 
-Special Codes:   1/C 
-
---------------------------------------------------------------------------------
-3. Registration Number:    TXu-961-904  
-Title:    AgentIdea application studio for IIS : version 1.0 / authors, Grant Steinfeld, Oren Kredo. 
-Description:    Computer program. 
-Note:    Printout only deposited. 
-Claimant:    cAgentIdea, LLC 
-Created:    2000 
-
-Registered:    10Jul00
-
-Special Codes:   1/C 
-
-to verify search for Grant Steinfeld or Oren Kredo
-http://www.copyright.gov/records/cohm.html
-
-*/
+ */
+ 
 
 var secs = 30;
 var timerID = null;
@@ -68,10 +23,27 @@ function InitializeTimer(callback,aSecs,aRepeat,aDelay)
     delay = aDelay;
 }
   
+  
+  function SetDelay(t)
+  {
+    delay = t;
+  
+  }
+  
   function StopTheClock()
   { 
     if(timerRunning)
-     clearTimeout(timerID);
+    {
+         try
+         {
+            clearTimeout(timerID);
+            //alert("cleared timer " + timerID);
+         }
+         catch(errrr)
+         {
+            alert("stop clock error" + errrr.description);
+         }
+     }
      
     timerRunning = false;
   }
